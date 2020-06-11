b72667d8d6e3a92bf75915fd482bc1cd5c00dc62
vijay <vijayyadav1002@gmail.com>
2020-06-11 00:25:16 -0500

    Added Styled component

--- templates/react-redux-todo/src/todo.js
+++ templates/react-redux-todo/src/todo.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import TodoList from './connect/todo-list.connect';
 import TodoInput from './connect/todo-input.connect';
+import styled from 'styled-components';
+
+const TodoWrapper = styled.div`
+    margin: 5rem auto 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Heading = styled.h1`
+    font-size: 3rem;
+`;
 
 const Todo = () => (
-  <>
+  <TodoWrapper>
+    <Heading>Things to do</Heading>
     <TodoInput />
     <TodoList />
-  </>
+  </TodoWrapper>
 );
 
 export default Todo;
