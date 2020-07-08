5d4d32bdccd762b57ee5a6eb36cfcbbeb18bbca5
vijay <vijayyadav1002@gmail.com>
2020-07-08 15:54:58 -0500

    Updated test in boilerplate

--- templates/react-jest-parcel-boilerplate/__test__/src/counter/counter.test.js
+++ templates/react-jest-parcel-boilerplate/__test__/src/counter/counter.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import { Counter } from '../../../src/counter/counter';
+jest.useFakeTimers();
 
 describe('<Counter/>', () => {
   it('should render correctly', () => {
     const component = shallow(<Counter />);
-
-    expect(component).toMatchSnapshot();
+    expect(setTimeout).toHaveBeenCalled();
+    expect(toJson(component)).toMatchSnapshot();
   });
 });
--- templates/react-jest-parcel-boilerplate/jest.config.js
+++ templates/react-jest-parcel-boilerplate/jest.config.js
@@ -1,11 +1,26 @@
 module.exports = {
+  verbose: true,
   setupFiles: ["./setup-test.js"],
+  testEnvironment: "node",
+  globals: {
+    "NODE_ENV": "test"
+  },
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json'
+  ],
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
   },
   transform: {
-    '^.+\\.js$': 'babel-jest'
+    "^.+\\.[t|j]sx?$": "babel-jest"
   },
-  transformIgnorePatterns: ["<rootDir>/node_modules/"]
+  testURL: 'http://localhost/',
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.js"],
+  coverageReporters: ["html", "text-summary"]
 };
